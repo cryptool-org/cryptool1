19fa6984a4093e99696449221a1a0be7bc1873ba
koy <>
2001-01-30 15:10:24 +0000

    no message

---
+++ setup/setup.bat
@@ -0,0 +1,4 @@
+cd ..\..\release\install
+cp ..\..\source\multipad\Release\CrypTool.exe .
+cp ..\..\source\multipad\Release\CrypTool.hlp .
+"C:\Programme\Setup Factory 5.0\Builder.exe" "..\..\source\setup\CrypTool.sf5"
