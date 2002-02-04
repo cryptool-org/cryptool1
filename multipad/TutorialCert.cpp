d75ffdfbda5a8ab398b143984c39e1e21c4f8d3f
o9d100 <>
2002-02-04 15:05:39 +0000

    cert step 1

--- multipad/TutorialCert.cpp
+++ multipad/TutorialCert.cpp
@@ -89,65 +89,30 @@ BOOL CTutorialCert::SetPublicKey( CString &sKeyPublic, int base )
 // Zur dauerhaften Speicherung: RSA-KEY in der PSE ablegen
 BOOL CTutorialCert::InitSecude()
 {
-	KeyBits		kbSecret;
-	KeyBits		kbPublic;
-	KeyInfo		kiSecret;
-	KeyInfo		kiPublic;
-	BitString*  bsSecret;
-	BitString*  bsPublic;
-	PSE			m_hPSE;
-	CString		sPseName;
-	CString		sUserKeyId;
-	char*		szPin;
-	char*		szPseName;
-	OctetString* osKey;
-	int			fehler;
-	
-	/*		
-		// Generate RSA key pair
-		Algorithmus = theApp.SecudeLib.md5WithRsaEncryption_aid;
-		verfahren = RSA_KEYFILE_IDSTRING;
-		keyBits = m_rsa_modul_str; // used keysize as string
-	*/
+	PSE				m_hPSE;
+	CString			sPseName;
+	CString			sUserKeyId;
+	char*			szPin;
+	char*			szPseName;
+	PSESel*			psesel;
 
+	KeyBits			kbSecret;
+	KeyInfo			kiSecret;
+	BitString*		bsSecret;
+	OctetString*	osSecret;
 
-	// Initialisierung
-	memset(&kiSecret, 0, sizeof(KeyInfo));
-	memset(&kbSecret, 0, sizeof(KeyBits));
-	memset(&kiPublic, 0, sizeof(KeyInfo));
-	memset(&kbPublic, 0, sizeof(KeyBits));
+	KeyBits			kbPublic;
+	KeyInfo			kiPublic;
+	BitString*		bsPublic;
 
-	// Allokation
-	kbSecret.part1.noctets = bits(m_bigPrime_p)/8+1;
-	kbSecret.part1.octets = new char[kbSecret.part1.noctets];
-	kbSecret.part2.noctets = bits(m_bigPrime_q)/8+1;
-	kbSecret.part2.octets = new char[kbSecret.part2.noctets];
-	kbPublic.part1.noctets = bits(N)/8+1;
-	kbPublic.part1.octets = new char[kbPublic.part1.noctets];
-	kbPublic.part2.noctets = bits(e)/8+1;
-	kbPublic.part2.octets = new char[kbPublic.part2.noctets];
+	int*			keysize;
 
-	// GEHEIMEN SCHLÜSSEL KONVERTIEREN IN 3 SCHRITTEN
-	// 1.Schritt: Konvertierung von Big p und Big q nach KeyBits
-	ASSERT(static_cast<unsigned>(to_binary(m_bigPrime_p, kbSecret.part1.noctets, kbSecret.part1.octets))==kbSecret.part1.noctets);
-	ASSERT(static_cast<unsigned>(to_binary(m_bigPrime_q, kbSecret.part2.noctets, kbSecret.part2.octets))==kbSecret.part2.noctets);
-	kbSecret.choice = 2;	
-	// 2.Schritt: Konvertierung von KeyBits nach KeyInfo (ASN.1 Format)
-	kiSecret.subjectAI = theApp.SecudeLib.rsa_aid; // RSA Algorithmus-ID
-	ASSERT(bsSecret = theApp.SecudeLib.e_KeyBits(&kbSecret));
-	kiSecret.subjectkey = *bsSecret;
-	// 3.Schritt: Konvertierung von KeyInfo nach OctetString
-	ASSERT(osKey = theApp.SecudeLib.e_KeyInfo( &kiSecret ));
-
-	// ÖFFENTLICHEN SCHLÜSSEL KONVERTIEREN IN 2 SCHRITTEN
-	// 1.Schritt: Konvertierung von Big N und Big e nach KeyBits
-	ASSERT(static_cast<unsigned>(to_binary(N, kbPublic.part1.noctets, kbPublic.part1.octets))==kbPublic.part1.noctets);
-	ASSERT(static_cast<unsigned>(to_binary(e, kbPublic.part2.noctets, kbPublic.part2.octets))==kbPublic.part2.noctets);
-	kbPublic.choice = 2;
-	// 2.Schritt: Konvertierung von KeyBits nach KeyInfo (ASN.1 Format)
-	kiPublic.subjectAI = theApp.SecudeLib.rsa_aid; // RSA Algorithmus-ID
-	ASSERT(bsPublic = theApp.SecudeLib.e_KeyBits(&kbPublic));
-	kiPublic.subjectkey = *bsPublic;
+	int				fehler;
+	int				pBit, qBit, NBit, eBit;
+	pBit = bits(m_bigPrime_p);
+	qBit = bits(m_bigPrime_q);
+	NBit = bits(N);
+	eBit = bits(e);
 
 	// PSE-Namen erstellen
 	sUserKeyId.Format(IDS_CREATE_USER_KEY_ID2, m_sName, m_sFirstName, TUTORIAL_ALG_NAME, GetBitLength(), time(&m_lTime), TUTORIAL_KEY_ID);
@@ -160,120 +125,74 @@ BOOL CTutorialCert::InitSecude()
 	strcpy(szPin, LPCTSTR(m_sPIN));
 	ASSERT(m_hPSE = theApp.SecudeLib.af_create(szPseName, NULL, szPin, NULL, TRUE));
 
-	/*
-	m_Key.alg = theApp.SecudeLib.rsa_aid;
-	m_Key.key_size = GetBitLength();
-	m_Key.key=NULL;
-	m_Key.pse_sel=NULL;
-	m_Key.add_object=NULL;
-	m_Key.add_object_type=NULL;
-	m_Key.private_key=NULL;
-	ASSERT(!theApp.SecudeLib.af_gen_key (m_hPSE, &m_Key, SIGNATURE, 1));
-	*/
 
-	// Schlüssel-Objekt-ID finden
-	m_hPSE->psesel->object = "SKnew"; 
-	m_hPSE->psesel->object_type = theApp.SecudeLib.af_get_objoid(m_hPSE->psesel->object);
-	// Privaten Schlüssel in die PSE schreiben
-	fehler = theApp.SecudeLib.sec_write_PSE (m_hPSE->psesel, osKey);
+	memset(&kbSecret, 0, sizeof(KeyBits));
+	kbSecret.part1.noctets = (bits(m_bigPrime_p)+7)/8;
+	kbSecret.part1.octets = new char[kbSecret.part1.noctets];
+	kbSecret.part2.noctets = (bits(m_bigPrime_q)+7)/8;
+	kbSecret.part2.octets = new char[kbSecret.part2.noctets];
+	ASSERT(static_cast<unsigned>(to_binary(m_bigPrime_p, kbSecret.part1.noctets, kbSecret.part1.octets))==kbSecret.part1.noctets);
+	ASSERT(static_cast<unsigned>(to_binary(m_bigPrime_q, kbSecret.part2.noctets, kbSecret.part2.octets))==kbSecret.part2.noctets);
 
+	bsSecret = theApp.SecudeLib.e_KeyBits(&kbSecret);
+
+	memset(&kiSecret, 0, sizeof(KeyInfo));
+	kiSecret.subjectkey.nbits = bsSecret->nbits;
+	kiSecret.subjectkey.bits = bsSecret->bits;
+	bsSecret->bits = NULL;	
+	kiSecret.subjectAI = theApp.SecudeLib.aux_cpy_AlgId (theApp.SecudeLib.rsa_aid);
+	keysize = reinterpret_cast<int*>(kiSecret.subjectAI->param);
+	*keysize = (kbSecret.part1.noctets + kbSecret.part2.noctets)*8;
+
+	//(reinterpret_cast<int*>(kiSecret.subjectAI->param))* = kbSecret.part1.noctets + kbSecret.part2.noctets;
+
+	// write private key to PSE
+	psesel = theApp.SecudeLib.af_get_PSESel(m_hPSE, static_cast<ObjId*>(0) );
+	psesel->object = theApp.SecudeLib.aux_cpy_String(SKnew_name);
+	psesel->object_type = theApp.SecudeLib.SKnew_oid;
+	osSecret = theApp.SecudeLib.e_KeyInfo(&kiSecret);
+	fehler = theApp.SecudeLib.sec_write_PSE(psesel, osSecret);
+	theApp.SecudeLib.aux_free_OctetString(&osSecret);
 	
-	OctetString* os1;
-	fehler = theApp.SecudeLib.sec_read(m_hPSE->psesel, os1);
-	KeyInfo* ki1= theApp.SecudeLib.d_KeyInfo(os1);
 	
-
-	/*
-	// Öffentlichen Schlüssel ersetzen
-	theApp.SecudeLib.aux_free_KeyInfo(&(m_Key.key));
-	m_Key.key = &kiPublic;
-	*/
-	
-	// Zertifikat
-	CString sDisName;
-	DName*	dname;
-	sDisName.Format(IDS_CREATE_DISNAME, m_sFirstName, m_sName, m_lTime);
-	char*	szDName = new char[sDisName.GetLength()+1];
-	strcpy(szDName, LPCTSTR(sDisName));
-
-	ASSERT(dname = theApp.SecudeLib.aux_Name2DName(szDName));
-	
-
-	/*
+	// Initialisierung der Variablen, die zur Zertifizierung notwendig sind
 	OctetString osSerial;
 	char *string="000001";
 	osSerial.noctets=strlen(string);
-	osSerial.octets = new char[osSerial.noctets];
-	strcpy(osSerial.octets, string);
-	*/
-	
+	osSerial.octets=string;
+
+	CString sDisName;
+	DName*	dname;
+	sDisName.Format(IDS_CREATE_DISNAME, m_sFirstName, m_sName, m_lTime);
+	LPTSTR	szDName = new TCHAR[sDisName.GetLength()+1];
+	_tcscpy(szDName, LPCTSTR(sDisName));
+	ASSERT(dname = theApp.SecudeLib.aux_Name2DName(szDName));
+
+	memset(&kbPublic, 0, sizeof(KeyBits));
+	kbPublic.part1.noctets = (bits(N)+7)/8;
+	kbPublic.part1.octets = new char[kbPublic.part1.noctets];
+	kbPublic.part2.noctets = (bits(e)+7)/8;
+	kbPublic.part2.octets = new char[kbPublic.part2.noctets];
+	ASSERT(static_cast<unsigned>(to_binary(N, kbPublic.part1.noctets, kbPublic.part1.octets))==kbPublic.part1.noctets);
+	ASSERT(static_cast<unsigned>(to_binary(e, kbPublic.part2.noctets, kbPublic.part2.octets))==kbPublic.part2.noctets);
+
+	bsPublic = theApp.SecudeLib.e_KeyBits(&kbPublic);
+
+	memset(&kiPublic, 0, sizeof(KeyInfo));
+	kiPublic.subjectkey.nbits = bsPublic->nbits;
+	kiPublic.subjectkey.bits = bsPublic->bits;
+	bsPublic->bits = NULL;
+	kiPublic.subjectAI = theApp.SecudeLib.aux_cpy_AlgId (theApp.SecudeLib.rsa_aid);
+	keysize = reinterpret_cast<int*>(kiPublic.subjectAI->param);
+	*keysize = (kbSecret.part1.noctets + kbSecret.part2.noctets)*8;
 
 	Certificate* Cert;
-	//ASSERT(Cert=theApp.SecudeLib.af_create_Certificate( m_hPSE, m_Key.key, theApp.SecudeLib.md5WithRsaEncryption_aid, "SKnew", dname, &osSerial, NULL, NULL, TRUE, NULL));
-	/*
-	// Make it a X509.v3 certificate
-	// Add extensions, in particular store the PSE name in a private extension.
-	// This is required if the certificate is exported via PKCS#12 and later
-	// imported by another CrypTool
-
-	// get PSE name
-	LPTSTR UserKeyId_str= new TCHAR[sUserKeyId.GetLength()+1];
-	_tcscpy(UserKeyId_str, sUserKeyId);
-
-	SEQUENCE_OF_Extension extensions;
-	extensions.element = (v3Extension *) theApp.SecudeLib.aux_malloc(sizeof(v3Extension));
-	extensions.element->extnId = CrypToolPSEName_OID();
-	extensions.element->critical = false;
-	extensions.element->extnDERcode = theApp.SecudeLib.e_PrintableString((char *) UserKeyId_str);
-	extensions.next = NULL;
-	delete [] UserKeyId_str;
-
-	CertExtensions cert_ext;
-	memset (&cert_ext, 0, sizeof(CertExtensions));
-	cert_ext.nonSupported = &extensions;
-
-	Cert->tbs->extensions = &cert_ext;
-
-	PSE hPSE_CA;
-	ASSERT(hPSE_CA=theApp.SecudeLib.af_open(CaPseDatei, CaPseVerzeichnis, PSEUDO_MASTER_CA_PINNR, NULL));
-	Certificate* Cert2;
-	ASSERT(Cert2=theApp.SecudeLib.af_certify(hPSE_CA, Cert, NULL, theApp.SecudeLib.md5WithRsaEncryption_aid, NULL));
-
-	//Zertifikat in die CA-Datenbank einfügen!!!
-	ASSERT(!theApp.SecudeLib.af_cadb_add_Certificate (hPSE_CA, SIGNATURE, Cert2));
-
-	PKRoot* pkroot;
-	VERIFY(pkroot = theApp.SecudeLib.af_pse_get_PKRoot(hPSE_CA));
-	if(pkroot)
-	{
-		ASSERT(!theApp.SecudeLib.af_pse_update_PKRoot(m_hPSE, pkroot));
-	}
-	theApp.SecudeLib.aux_free_PKRoot(&pkroot);
-	
-	ASSERT(!theApp.SecudeLib.af_pse_update(m_hPSE, "Cert", Cert2, *theApp.SecudeLib.Cert_OID));
+	ASSERT(Cert=theApp.SecudeLib.af_create_Certificate(m_hPSE, &kiPublic, theApp.SecudeLib.md5WithRsaEncryption_aid, "SKnew", dname, &osSerial, NULL, NULL, TRUE, NULL));
 
 
-	/*
-	AlgId* Algorithmus = theApp.SecudeLib.md5WithRsaEncryption_aid;
-	m_Key.alg = theApp.SecudeLib.rsa_aid;
-	m_Key.key_size = 512; // used keysize as int
-	char* verfahren = RSA_KEYFILE_IDSTRING;
-	char* keyBits = "512"; // used keysize as string
-	*/
+	fehler = theApp.SecudeLib.af_close(m_hPSE);
 
-	// Delokation
-	theApp.SecudeLib.sec_close(m_hPSE->psesel);
-	//theApp.SecudeLib.af_close(hPSE_CA);
-	theApp.SecudeLib.aux_free_DName(&dname); 
-	//theApp.SecudeLib.aux_free_BitString(&bsSecret); 
-	//theApp.SecudeLib.aux_free_OctetString(&osKey);
-	//delete[] kbSecret.part1.octets;
-	//delete[] kbSecret.part2.octets;
-	delete[] szPin;
-	delete[] szPseName;
-	delete[] szDName;
-
-	return TRUE;
+	return TRUE;	
 }
 
 							  
