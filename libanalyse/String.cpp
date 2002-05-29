9ce42dc164eb6a8c35f631e68096d67ebb62b021
idj100 <>
2002-05-29 15:08:36 +0000

    outcomment of << // #include "builtin.h" >>

--- libanalyse/String.cpp
+++ libanalyse/String.cpp
@@ -27,7 +27,7 @@ Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include <ctype.h>
 #include <limits.h>
 #include "new.h"
-#include "builtin.h"
+// #include "builtin.h"
 
 #undef OK
 
