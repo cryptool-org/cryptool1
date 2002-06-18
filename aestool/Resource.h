336b0a03e7b0c5bb6dd2ab292269b4fd4bde0347
bdf100 <>
2002-06-18 12:25:31 +0000

    aes tool comments be

--- aestool/Resource.h
+++ aestool/Resource.h
@@ -39,7 +39,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_CONTROL_VALUE         1015
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
