7fad674b87c3b7d75642f0eae27c797251db7a1e
koy <>
2005-12-29 14:14:02 +0000

    Base64 En-/Decoding

---
+++ CrypTool/base64.cpp
@@ -0,0 +1,427 @@
+/*********************************************************************
+
+Copyright (C) Deutsche Bank AG 1998-2003, Frankfurt am Main
+Copyright (C) Universität Siegen und Darmstadt
+
+This file is part of CrypTool.
+
+CrypTool is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+Foobar is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with CrypTool; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+In addition, as a special exception, Secude GmbH gives permission to
+link the code of this program with the library SECUDE (or with
+modified versions of SECUDE that use the same license as SECUDE), and
+distribute linked combinations including the two. You must obey the
+GNU General Public License in all respects for all of the code used
+other than SECUDE. If you modify this file, you may extend this
+exception to your version of the file, but you are not obligated to do
+so. If you do not wish to do so, delete this exception statement from
+your version.
+ 
+In addition, as a special exception, Shamus Software Ltd. gives
+permission to link the code of this program with the library libmiracl
+(or with modified versions of libmiracl that use the same license as
+libmiracl), and distribute linked combinations including the two. You
+must obey the GNU General Public License in all respects for all of
+the code used other than libmiracl. If you modify this file, you may
+extend this exception to your version of the file, but you are not
+obligated to do so. If you do not wish to do so, delete this exception
+statement from your version.
+
+**********************************************************************/
+
+/*
+ * base64.c
+ *
+ * Implementation of base64 encoding/decoding algorithm. 
+ *
+ * Copyright (c) 2001--2002 by Wolfgang Wieser (wwieser@gmx.de)
+ *
+ * This file may be distributed and/or modified under the terms of the 
+ * GNU General Public License version 2 as published by the Free Software 
+ * Foundation. (See COPYING.GPL for details.)
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+//////////////////////////////////////////////////////////////
+// Copyright 1998-2000 Deutsche Bank AG, Frankfurt am Main	//
+//////////////////////////////////////////////////////////////
+#include "stdafx.h"
+#include "CryptDoc.h"
+#include "FileTools.h"
+#include "Cryptography.h"
+#include <ios>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "ScintillaDoc.h"
+#include "ScintillaView.h"
+
+
+#define STREAM_IN_BLOCKLENGTH_ENC 4080  //Größe des Einlesebuffers beim Enkodieren
+#define STREAM_OUT_BLOCKLENGTH_ENC (STREAM_IN_BLOCKLENGTH_ENC*4/3+STREAM_IN_BLOCKLENGTH_ENC/48*1)  //Größe des Auslesebuffers beim Enkodieren
+#define STREAM_IN_BLOCKLENGTH_DEC 4096   //Größe des Einlesebuffers beim Dekodieren
+#define STREAM_OUT_BLOCKLENGTH_DEC STREAM_IN_BLOCKLENGTH_DEC*3/4  //Größe des Auslesebuffers beim Dekodieren
+
+
+
+
+#define HLIB_IN_HLIB 1
+#include "base64.h"
+
+/* Maps the values in range 0...63 to base64 chars. */
+static const unsigned char *_base64_encode=(unsigned char *)
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	"abcdefghijklmnopqrstuvwxyz"
+	"0123456789+/";
+
+ssize_t Base64Encode(
+	const char *_in,size_t inlen,
+	char *_out,size_t outlen,
+	u_int32_t *state/*=NULL*/,int lwidth/*=0*/,ssize_t *outfill)
+{
+	unsigned char *in=(unsigned char *)_in;
+	unsigned char *out=(unsigned char *)_out;
+	unsigned char *inend=in+inlen;
+	unsigned char *outend=out+outlen;
+	u_int32_t acc;
+	int ai,ci;
+
+	if(state)
+	{
+		acc=(*state)&0xffffU;
+		ai=((*state)>>16)&0x3U;
+		ci=((*state)>>18);
+	}
+	else
+	{  acc=0;  ai=0;  ci=0;  }
+	
+	for(;in<inend;in++)
+	{
+		acc<<=8;
+		acc|=(u_int32_t)(*in);
+		++ai;
+		if(ai==3)
+		{
+			*(out++)=_base64_encode[(acc>>18) & 0x3fU];
+			*(out++)=_base64_encode[(acc>>12) & 0x3fU];
+			*(out++)=_base64_encode[(acc>> 6) & 0x3fU];
+			*(out++)=_base64_encode[ acc      & 0x3fU];
+			ai=0;
+			acc=0U;
+			if(lwidth)  if((++ci)==lwidth)
+			{
+				*((char*)(out++))='\n';
+				ci=0;
+			}
+		}
+	}
+	
+	if(state && inlen)
+	{
+		/* save carry in state (ai=0,1,2) */
+		*state = 
+			(((u_int32_t)ci)<<18) | 
+			(((u_int32_t)ai)<<16) | 
+			acc;
+	}
+	else
+	{
+			if(state)  *state=0;
+	
+		/* store rest of input in output */
+		/* ai==3 never possible here.*/
+		switch(ai)
+		{
+			case 0:
+				break; //Wenn case 0 dann encoding vollständig.
+			case 1:
+				*(out++)=_base64_encode[(acc>>2) & 0x3fU];
+				*(out++)=_base64_encode[(acc<<4) & 0x3fU];
+				*((char*)(out++))='=';
+				*((char*)(out++))='=';
+				break;
+			case 2:
+				*(out++)=_base64_encode[(acc>>10) & 0x3fU];
+				*(out++)=_base64_encode[(acc>> 4) & 0x3fU];
+				*(out++)=_base64_encode[(acc<< 2) & 0x3fU];
+				*((char*)(out++))='=';
+				break;
+		}	
+	}
+    *outfill = out-(unsigned char*)_out;
+	return(B64_CODE_NOERROR);
+}
+
+static const unsigned char *_base64_decode=(const unsigned char *)
+	"\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xfe\xfe\xfe\xfe\xfe\xff\xff"
+	"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+	"\xfe\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x3e\xff\xff\xff\x3f"
+	"\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\xff\xff\xff\xfd\xff\xff"
+	"\xff\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e"
+	"\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\xff\xfe\xff\xff\xff"
+	"\xff\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28"
+	"\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\xff\xff\xff\xff\xfe";
+
+ssize_t Base64Decode(
+	const char *_in,size_t inlen,char *_out,size_t outlen
+	,u_int32_t *state /*=NULL*/,ssize_t *outfill,ssize_t *indec, BOOL lastBlock)
+{
+	unsigned char *ib=(unsigned char*)_in;
+	unsigned char *ib_end=ib+inlen;
+	unsigned char *ob=(unsigned char*)_out;
+	unsigned char *ob_end=ob+outlen;
+	u_int32_t dec=0xfdU;   /* simulate end char in case inlen=0 */
+	u_int32_t acc = state ? ((*state)&0xffffffU) : 0U;
+	int       ai  = state ? ((*state)>>24) : 0;
+	unsigned int zeichen=0;
+	CString result="";
+
+	BOOL illegal_eq = FALSE;
+
+
+	for(; ib<ib_end; ib++)
+	{		
+		zeichen++;
+
+		if((*ib)>=128U)  
+		{
+			*indec=(char*)ib-_in;
+			*outfill = (char*)ob-_out;
+			return(B64_DECODE_ILLEGAL_CHARACTER);
+		}
+		dec=_base64_decode[(int)(*ib)];
+		if(dec<64)
+		{
+			if (illegal_eq)
+			{
+				*indec=(char*)ib-_in;
+				*outfill = (char*)ob-_out;
+				return(B64_DECODE_ILLEGAL_CHARACTER);				
+			}
+			acc=(acc<<6)|dec;
+			++ai;
+			if(ai==4)  /* got 4 chars */
+			{
+				*(ob++)=(unsigned char)((acc>>16) & 0xffU);
+				*(ob++)=(unsigned char)((acc>> 8) & 0xffU);
+				*(ob++)=(unsigned char)( acc      & 0xffU);
+				acc=0U;  ai=0;
+			}
+		}
+		else switch(dec)
+		{
+			case 0xffU:  
+					*indec=(char*)ib-_in;
+					*outfill = (char*)ob-_out;
+					return(B64_DECODE_ILLEGAL_CHARACTER);
+					break;
+			case 0xfdU:
+					*indec=(char*)ib-_in;
+					if (!lastBlock)
+					{
+						*outfill = (char*)ob-_out;
+						return(B64_DECODE_PREMATURE_END);
+					}
+					else
+						illegal_eq = TRUE;
+					break;
+					// goto breakfor;
+		}
+		
+	}
+	breakfor:;
+	/* ai==4 not possible here. ai<4 here. */
+	
+	if(state && dec!=0xfdU)  /* state and no end char */
+	{
+		/* Save carry in state. */
+		*state = (((u_int32_t)ai)<<24) | acc;
+	}
+	else
+	{
+		if(state)  *state=0U;
+		/* Save the last bytes in the output buffer. */
+
+		switch(ai)
+		{
+			case 3:  /* got 3 chars */
+				if ( acc & 0x03 ) return (B64_DECODE_MISSING_CHAR); //FIXME ???
+				*(ob++)=(unsigned char)((acc>>10) & 0xffU);
+				*(ob++)=(unsigned char)((acc>> 2) & 0xffU);
+				break;
+			case 2:  /* got 2 chars */
+				if ( acc & 0x0f ) return (B64_DECODE_MISSING_CHAR);// FIXME ???
+				*(ob++)=(unsigned char)((acc>> 4) & 0xffU);
+				break;
+			case 1: 
+				return (B64_DECODE_MISSING_CHAR);
+				break; //FIXME??
+				
+			}
+		}
+	*outfill = (char*)ob-_out;
+	return(B64_CODE_NOERROR);
+}
+
+
+void dobase64enc(const char *infile, const char *OldTitle)
+{
+	char outfile[CRYPTOOL_PATH_LENGTH], title[256];
+    CAppDocument *NewDoc;
+
+    GetTmpName(outfile,"cry",".tmp");
+
+	int lwidth=16; //gewünschte Zeilenlänge/4=lwidth
+	ssize_t rv;
+	int count;
+	char buffer[STREAM_IN_BLOCKLENGTH_ENC];
+	char ob[STREAM_OUT_BLOCKLENGTH_ENC];
+	unsigned int state=0;
+	ssize_t outchars=0;
+	bool lf= false;
+
+	std::fstream filein(infile, std::ios_base::in|std::ios_base::binary);
+	if (!filein.is_open())
+	{
+		base64error( outfile,NULL,B64_FILE_OPEN_ERROR,NULL);
+		return;
+	}
+	std::fstream fileout(outfile, std::ios_base::out|std::ios_base::binary);
+	
+	if (!fileout.is_open())
+	{
+		base64error( outfile,NULL,B64_FILE_OPEN_ERROR,NULL);
+		return;
+	}																						
+
+	do
+	{
+		filein.read(buffer, STREAM_IN_BLOCKLENGTH_ENC);
+		count = filein.gcount();
+		rv=Base64Encode(buffer,count,ob,STREAM_OUT_BLOCKLENGTH_ENC,&state,lwidth, &outchars);    
+			
+		if(rv>=0)
+		{			
+		
+			if(outchars)
+				lf=(ob[outchars-1]=='\n') ? true : false;
+
+			fileout.write(ob,outchars);
+			if (!fileout.good())
+			{
+				base64error(outfile,NULL,B64_FILE_WRITE_ERROR,NULL);		
+				return;
+			}
+		}		
+	}
+	while (count);
+
+	if (lf==false)
+	{
+		ob[0]='\n';
+		fileout.write(ob,1);
+	}
+
+	filein.close();
+	fileout.close();
+
+
+    NewDoc = theApp.OpenDocumentFileNoMRU(outfile);
+    remove(outfile);
+    if(NewDoc) 
+	{
+		LoadString(AfxGetInstanceHandle(),IDS_STRING_MSG_BASE64ENC_OF,pc_str,STR_LAENGE_STRING_TABLE);
+        MakeNewName(title,sizeof(title),pc_str,OldTitle);
+        NewDoc->SetTitle(title);
+    }
+
+	CView* CView_hilf = ((CMDIFrameWnd*)theApp.m_pMainWnd)->MDIGetActive()->GetActiveView();
+	((CScintillaView*)CView_hilf)->OnViewFontCourier10();
+	
+}
+
+void dobase64dec(const char *infile, const char *OldTitle)
+{
+	char outfile[CRYPTOOL_PATH_LENGTH], title[256];
+    CAppDocument *NewDoc;
+
+    GetTmpName(outfile,"cry",".txt");
+	ssize_t rv;
+	int count;
+	char buffer[STREAM_IN_BLOCKLENGTH_DEC];
+	char ob[STREAM_OUT_BLOCKLENGTH_DEC];
+	unsigned int state=0;
+	ssize_t outchars=0;
+	ssize_t inchars=0;
+	int zeichen=0;
+	
+	std::fstream filein(infile, std::ios_base::in|std::ios_base::binary);
+	if (!filein.is_open())
+	{
+		base64error(infile,buffer[inchars],B64_FILE_OPEN_ERROR,inchars);
+		return;
+	}
+	
+	std::fstream fileout(outfile, std::ios_base::out|std::ios_base::binary);
+	if (!fileout.is_open())
+	{
+		base64error( outfile,buffer[inchars],B64_FILE_OPEN_ERROR,inchars);
+		return;
+	}																							
+
+	do
+	{
+			
+		filein.read(buffer, STREAM_IN_BLOCKLENGTH_DEC);
+		count = filein.gcount();
+			
+			
+		rv=Base64Decode(buffer,count,ob,STREAM_OUT_BLOCKLENGTH_DEC,&state, &outchars, &inchars, filein.eof());
+			
+
+		if(rv<0)
+		{
+			base64error( "",buffer[inchars],rv,inchars+zeichen);	
+			count=0;
+		}
+		if(outchars>0)
+		{
+			fileout.write(ob,outchars);
+			if (!fileout.good())
+			{
+				base64error(outfile,NULL,B64_FILE_WRITE_ERROR,NULL);		
+				return;
+			}
+		}
+		zeichen=zeichen+4096;
+	}
+	while (count);
+
+	filein.close();
+	fileout.close();
+	
+    NewDoc = theApp.OpenDocumentFileNoMRU(outfile);
+    remove(outfile);
+    if(NewDoc) 
+	{
+		LoadString(AfxGetInstanceHandle(),IDS_STRING_MSG_BASE64DEC_OF,pc_str,STR_LAENGE_STRING_TABLE);
+        MakeNewName(title,sizeof(title),pc_str,OldTitle);
+        NewDoc->SetTitle(title);
+    }
+	
+} 
---
+++ CrypTool/base64error.cpp
@@ -0,0 +1,79 @@
+/*********************************************************************
+
+Copyright (C) Deutsche Bank AG 1998-2003, Frankfurt am Main
+Copyright (C) Universität Siegen und Darmstadt
+
+This file is part of CrypTool.
+
+CrypTool is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+Foobar is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with CrypTool; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+In addition, as a special exception, Secude GmbH gives permission to
+link the code of this program with the library SECUDE (or with
+modified versions of SECUDE that use the same license as SECUDE), and
+distribute linked combinations including the two. You must obey the
+GNU General Public License in all respects for all of the code used
+other than SECUDE. If you modify this file, you may extend this
+exception to your version of the file, but you are not obligated to do
+so. If you do not wish to do so, delete this exception statement from
+your version.
+ 
+In addition, as a special exception, Shamus Software Ltd. gives
+permission to link the code of this program with the library libmiracl
+(or with modified versions of libmiracl that use the same license as
+libmiracl), and distribute linked combinations including the two. You
+must obey the GNU General Public License in all respects for all of
+the code used other than libmiracl. If you modify this file, you may
+extend this exception to your version of the file, but you are not
+obligated to do so. If you do not wish to do so, delete this exception
+statement from your version.
+
+**********************************************************************/
+
+
+#include "stdafx.h"
+#include "CrypToolApp.h"
+#include "base64.h"
+
+
+void base64error(std::string text, char errchar,ssize_t errcode, ssize_t position)
+{
+	char str[2]; 
+	str[0] = errchar; str[1] = '\0';
+	CString result="";
+
+	switch(errcode)
+	{	
+		case -1:
+			result.Format(IDS_BASE64_DECODE_PREMATURE_END, position, str);
+			AfxMessageBox(result, MB_OK);
+			break;
+		case -2: 
+			result.Format(IDS_BASE64_DECODE_ILLEGAL_CHARACTER, position, str);
+			AfxMessageBox(result, MB_OK);
+			break;
+		case -3:
+			result.Format(IDS_BASE64_DECODE_MISSING_CHARS,text);
+			AfxMessageBox(result, MB_OK);
+			break;
+		case -4:		
+			result.Format(IDS_BASE64_FILE_WRITE_ERROR,text);
+			AfxMessageBox(result, MB_OK);
+			break;
+		case -5:
+			result.Format(IDS_BASE64_FILE_OPEN_ERROR,text);
+			AfxMessageBox(result, MB_OK);
+			break;
+	}
+}
