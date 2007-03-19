ed7534f1560a411d852bf31ccfeb60a0b022be91
marchal <>
2007-03-19 13:33:33 +0000

    updated user interface in password quality meter

--- CrypTool/DlgPasswordQualityMeter.cpp
+++ CrypTool/DlgPasswordQualityMeter.cpp
@@ -1,62 +1,28 @@
-/*********************************************************************
-
-Copyright (C) Deutsche Bank AG 1998-2003, Frankfurt am Main
-Copyright (C) Universität Siegen und Darmstadt
-
-This file is part of CrypTool.
-
-CrypTool is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2 of the License, or (at your
-option) any later version.
-
-Foobar is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
-
-You should have received a copy of the GNU General Public License
-along with CrypTool; if not, write to the Free Software Foundation,
-Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
-In addition, as a special exception, Secude GmbH gives permission to
-link the code of this program with the library SECUDE (or with
-modified versions of SECUDE that use the same license as SECUDE), and
-distribute linked combinations including the two. You must obey the
-GNU General Public License in all respects for all of the code used
-other than SECUDE. If you modify this file, you may extend this
-exception to your version of the file, but you are not obligated to do
-so. If you do not wish to do so, delete this exception statement from
-your version.
- 
-In addition, as a special exception, Shamus Software Ltd. gives
-permission to link the code of this program with the library libmiracl
-(or with modified versions of libmiracl that use the same license as
-libmiracl), and distribute linked combinations including the two. You
-must obey the GNU General Public License in all respects for all of
-the code used other than libmiracl. If you modify this file, you may
-extend this exception to your version of the file, but you are not
-obligated to do so. If you do not wish to do so, delete this exception
-statement from your version.
-
-**********************************************************************/
-
-// DlgPasswordQualityMeter.cpp
+// DlgPasswordQualityMeter.cpp : Implementierungsdatei
+//
 
 #include "stdafx.h"
 #include "CrypToolApp.h"
 #include "DlgPasswordQualityMeter.h"
 
-IMPLEMENT_DYNAMIC(CDlgPasswordQualityMeter, CDialog)
+
+// CDlgPasswordQualityMeter-Dialogfeld
+
 CDlgPasswordQualityMeter::CDlgPasswordQualityMeter(CWnd* pParent /*=NULL*/)
 	: CDialog(CDlgPasswordQualityMeter::IDD, pParent)
 {
-	password = "";
+	password = "password";
 	showPassword = false;
-	passwordQualityKeePass = "";
-	passwordQualityMozilla = "";
-	passwordQualityPGP = "";
-	passwordQualityAverage = "";
+
+	intQualityKeePass = 0;
+	intQualityMozilla = 0;
+	intQualityPGP = 0;
+	intQualityAverage = 0;
+
+	stringQualityKeePass = "";
+	stringQualityMozilla = "";
+	stringQualityPGP = "";
+	stringQualityAverage = "";
 }
 
 CDlgPasswordQualityMeter::~CDlgPasswordQualityMeter()
@@ -66,72 +32,90 @@ CDlgPasswordQualityMeter::~CDlgPasswordQualityMeter()
 void CDlgPasswordQualityMeter::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CDlgPasswordQualityMeter)
+
+	DDX_Text(pDX, IDC_EDIT_PASSWORD, password);
 	DDX_Check(pDX, IDC_CHECK_SHOWPASSWORD, showPassword);
-	DDX_Control(pDX, IDC_EDIT_PASSWORD, controlEditPassword);
+
+	DDX_Text(pDX, IDC_QUALITY_KEEPASS, stringQualityKeePass);
+	DDX_Text(pDX, IDC_QUALITY_MOZILLA, stringQualityMozilla);
+	DDX_Text(pDX, IDC_QUALITY_PGP, stringQualityPGP);
+	DDX_Text(pDX, IDC_QUALITY_AVERAGE, stringQualityAverage);
+
 	DDX_Control(pDX, IDC_PROGRESS_KEEPASS, controlQualityKeePass);
 	DDX_Control(pDX, IDC_PROGRESS_MOZILLA, controlQualityMozilla);
 	DDX_Control(pDX, IDC_PROGRESS_PGP, controlQualityPGP);
 	DDX_Control(pDX, IDC_PROGRESS_AVERAGE, controlQualityAverage);
-	DDX_Text(pDX, IDC_EDIT_PASSWORD, password);
-	DDX_Text(pDX, IDC_QUALITY_KEEPASS, passwordQualityKeePass);
-	DDX_Text(pDX, IDC_QUALITY_MOZILLA, passwordQualityMozilla);
-	DDX_Text(pDX, IDC_QUALITY_PGP, passwordQualityPGP);
-	DDX_Text(pDX, IDC_QUALITY_AVERAGE, passwordQualityAverage);
-	//}}AFX_DATA_MAP
 }
-
-
-BEGIN_MESSAGE_MAP(CDlgPasswordQualityMeter, CDialog)
-	//{{AFX_MSG_MAP(CDlgPasswordQualityMeter)
-	ON_EN_CHANGE(IDC_EDIT_PASSWORD, EditPasswordChanged)
-	ON_BN_CLICKED(IDC_CHECK_SHOWPASSWORD, OnShowPassword)
-	//}}AFX_MSG_MAP
-END_MESSAGE_MAP()
-
-
-// CDlgPasswordQualityMeter-Meldungshandler
 
 BOOL CDlgPasswordQualityMeter::OnInitDialog()
 {
 	CDialog::OnInitDialog();
-
-	// some inits
-	controlQualityKeePass.SetRange(0, 100);
-	controlQualityKeePass.SetStep(1);
-	controlQualityKeePass.SetPos(0);
-	controlQualityMozilla.SetRange(0, 100);
-	controlQualityMozilla.SetStep(1);
-	controlQualityMozilla.SetPos(0);
-	controlQualityPGP.SetRange(0, 100);
-	controlQualityPGP.SetStep(1);
-	controlQualityPGP.SetPos(0);
-	controlQualityAverage.SetRange(0, 100);
-	controlQualityAverage.SetStep(1);
-	controlQualityAverage.SetPos(0);
+	
+	UpdateUserInterface();
 
 	UpdateData(false);
-
-	return FALSE;
-}
-
-void CDlgPasswordQualityMeter::OnShowPassword()
-{
-	UpdateData(true);
-
-	if(showPassword)
-		controlEditPassword.SetPasswordChar('*');
-	else
-		controlEditPassword.SetPasswordChar(0);
+	
+	return TRUE;  // Geben Sie TRUE zurück, außer ein Steuerelement soll den Fokus erhalten
 }
 
 void CDlgPasswordQualityMeter::EditPasswordChanged()
 {
 	UpdateData(true);
 
-	// todo...
-	controlQualityKeePass.StepIt();
-	controlQualityKeePass.StepIt();
+	// TODO: implement quality measurement here
+	intQualityKeePass = password.GetLength() + 1;
+	intQualityMozilla = password.GetLength() + 2;
+	intQualityPGP = password.GetLength() - 5;
+	intQualityAverage = (intQualityKeePass+intQualityMozilla+intQualityPGP)/3;
+	// TODO: implement quality measurement here
+
+	UpdateUserInterface();
+}
+
+void CDlgPasswordQualityMeter::UpdateUserInterface()
+{
+	// first scale down quality values if necessary
+	if(intQualityKeePass >= 100) intQualityKeePass = 100;
+	if(intQualityKeePass < 0) intQualityKeePass = 0;
+	if(intQualityMozilla >= 100) intQualityMozilla = 100;
+	if(intQualityMozilla < 0) intQualityMozilla = 0;
+	if(intQualityPGP >= 100) intQualityPGP = 100;
+	if(intQualityPGP < 0) intQualityPGP = 0;
+	if(intQualityAverage >= 100) intQualityAverage = 100;
+	if(intQualityAverage < 0) intQualityAverage = 0;
+
+	// update (string) quality display
+	_itoa(intQualityKeePass, pc_str, 10);
+	stringQualityKeePass = pc_str;
+	stringQualityKeePass.Append(" %");
+	_itoa(intQualityMozilla, pc_str, 10);
+	stringQualityMozilla = pc_str;
+	stringQualityMozilla.Append(" %");
+	_itoa(intQualityPGP, pc_str, 10);
+	stringQualityPGP = pc_str;
+	stringQualityPGP.Append(" %");
+	_itoa(intQualityAverage, pc_str, 10);
+	stringQualityAverage = pc_str;
+	stringQualityAverage.Append(" %");
+
+	// update (progress bar) quality display
+	controlQualityKeePass.SetRange(0, 100);
+	controlQualityKeePass.SetPos(intQualityKeePass);
+	controlQualityMozilla.SetRange(0, 100);
+	controlQualityMozilla.SetPos(intQualityMozilla);
+	controlQualityPGP.SetRange(0, 100);
+	controlQualityPGP.SetPos(intQualityPGP);
+	controlQualityAverage.SetRange(0, 100);
+	controlQualityAverage.SetPos(intQualityAverage);
+	
 
 	UpdateData(false);
 }
+
+BEGIN_MESSAGE_MAP(CDlgPasswordQualityMeter, CDialog)
+	ON_EN_CHANGE(IDC_EDIT_PASSWORD, EditPasswordChanged)
+END_MESSAGE_MAP()
+
+
+
+// CDlgPasswordQualityMeter-Meldungshandler
