ef9f0f176fdabed2aabc3685b8bca46460dac079
marchal <>
2010-04-12 11:23:04 +0000

    a small fix in our CheckResourceIDs.pl script: I don't want to desperately search for a weird error in a couple of days

--- CrypTool/CheckResourceIDs.pl
+++ CrypTool/CheckResourceIDs.pl
@@ -229,7 +229,7 @@ foreach (sort keys %rcmenu) {
 }
 
 my @languages = sort keys %id;
-# @languages = (qw(ENGLISH GERMAN POLISH)); # FIXME
+# @languages = (qw(ENGLISH GERMAN POLISH SERBIAN SPANISH)); # FIXME
 my %idall = map { %$_ } values %id; # keys %ids: union of ids for all languages; values %ids: line number of one of the ids (any)
 # check IDs between languages
 foreach (sort keys %idall) {
