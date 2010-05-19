cd37704513d77eb1f5621a971654de9c55ab69bc
marchal <>
2010-05-19 14:57:58 +0000

    updated the authors.txt and its related dialog: the author's dialog looks identical in ALL languages now (no more bad-looking inconsistencies)

--- CrypTool/DlgAuthors.cpp
+++ CrypTool/DlgAuthors.cpp
@@ -45,7 +45,7 @@ static char THIS_FILE[] = __FILE__;
 #endif
 
 // this is a static sha-1 hash value (used for integrity of the authors.txt file)
-static const CString authorsFileHash = "5324df1e393d06427f036a59fd640d31415a2ae7";
+static const CString authorsFileHash = "f05b66970a336ff3df403cece4e1e99b32b7cfe3";
 
 
 /////////////////////////////////////////////////////////////////////////////
