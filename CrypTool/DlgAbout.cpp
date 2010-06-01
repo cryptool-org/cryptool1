77822af7d72b5f04e41214c471ee4cee0984f86b
marchal <>
2010-06-01 16:10:34 +0000

    [i18n] changes in resource file (most notably: renamed menu entry from Crypt/Decrypt to Encrypt/Decrypt in EN); the HTML files in the online help as well as the popup menu txt file changed accordingly; also, bug fix in About dialog: date resolution was ambiguous, June was mapped to January

--- CrypTool/DlgAbout.cpp
+++ CrypTool/DlgAbout.cpp
@@ -40,11 +40,11 @@ namespace OPENSSL {
 #define YEAR ((((__DATE__ [7] - '0') * 10 + (__DATE__ [8] - '0')) * 10 \
 + (__DATE__ [9] - '0')) * 10 + (__DATE__ [10] - '0'))
 
-#define MONTH (__DATE__ [2] == 'n' ? 0 \
+#define MONTH (__DATE__ [1] == 'a' && __DATE__ [2] == 'n' ? 0 \
 : __DATE__ [2] == 'b' ? 1 \
 : __DATE__ [2] == 'r' ? (__DATE__ [0] == 'M' ? 2 : 3) \
 : __DATE__ [2] == 'y' ? 4 \
-: __DATE__ [2] == 'n' ? 5 \
+: __DATE__ [1] == 'u' && __DATE__ [2] == 'n' ? 5 \
 : __DATE__ [2] == 'l' ? 6 \
 : __DATE__ [2] == 'g' ? 7 \
 : __DATE__ [2] == 'p' ? 8 \
