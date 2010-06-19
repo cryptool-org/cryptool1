bc55952a52f358498e61b7b85401911316bafdd4
esslinger <>
2010-06-19 20:40:09 +0000

    Program "Number Theory" updated - English from 2010-06-19, German from 2009-11-08

--- setup/template/Bc/BCmin.bat
+++ setup/template/Bc/BCmin.bat
@@ -1,10 +1,10 @@
 
-IF EXIST Bc.exe GOTO OK
+IF EXIST BC.exe GOTO OK
 
 Z:
 cd ..
 cd \Zahlentheorie\BC
 
 :OK
-bc -lq prims.txt dislog.txt ecm.txt quadRest.txt calc.txt > res.txt
+bc -lq prims.txt calc.txt | find /V "Poll" | find /V "|" | sort > res.txt
 exit
