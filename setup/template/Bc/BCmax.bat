cc217a69dc55a15e33d161a3e89a9bfa204958b7
marchal <>
2012-02-19 17:38:21 +0000

    removed quadratic sieve from bc-based number theory program

--- setup/template/Bc/BCmax.bat
+++ setup/template/Bc/BCmax.bat
@@ -6,6 +6,7 @@ cd \Zahlentheorie\Bc
 
 :OK
 cls
-bc -lq prims.txt dislog.txt ecm.txt quadRest.txt sieb.txt seed
-
+@rem Feb2012: Sorted out quadratic sieve function within ZT tutorial, as bc with sieb.txt crashes under Win7
+@rem bc -lq prims.txt dislog.txt ecm.txt quadRest.txt sieb.txt seed
+bc -lq prims.txt dislog.txt ecm.txt quadRest.txt seed
 
