7f37a887d449af43c82a76229b0b45148660939d
marchal <>
2012-11-05 08:36:14 +0000

    minor fixes in resource file; moved build version to 1.4.31 Beta5a

--- CrypTool/updateBuildInformation.pl
+++ CrypTool/updateBuildInformation.pl
@@ -23,7 +23,7 @@ use strict;
 my $CrypToolVersionMajor = "1";
 my $CrypToolVersionMinor = "4";
 my $CrypToolVersionRevision = "31";
-my $CrypToolVersionAddition = "Beta 5";
+my $CrypToolVersionAddition = "Beta 5a";
 # THE LINES ABOVE CONTROL THE BUILD
 #
 
