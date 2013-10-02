6653a362419eec5401b85f90ea47762ab26e0fc0
marchal <>
2013-10-02 06:50:26 +0000

    moved CrypTool version from 1.4.31 Beta 6a to 1.4.31 Beta 6b

--- CrypTool/updateBuildInformation.pl
+++ CrypTool/updateBuildInformation.pl
@@ -23,7 +23,7 @@ use strict;
 my $CrypToolVersionMajor = "1";
 my $CrypToolVersionMinor = "4";
 my $CrypToolVersionRevision = "31";
-my $CrypToolVersionAddition = "Beta 6a";
+my $CrypToolVersionAddition = "Beta 6b";
 # THE LINES ABOVE CONTROL THE BUILD
 #
 
