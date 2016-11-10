14c47eddd2c9d054edbadd9b2947fe758712555d
esslinger <>
2016-11-10 13:29:14 +0000

    CT1: Added small comment and empty line at end in batch files

--- setup/template/Bc/BCmax.bat
+++ setup/template/Bc/BCmax.bat
@@ -7,6 +7,9 @@ cd \Zahlentheorie\Bc
 :OK
 cls
 @rem Feb2012: Sorted out quadratic sieve function within ZT tutorial, as bc with sieb.txt crashes under Win7
+@rem Nov2016: Worked under Win10 both directly calling bcmax and from within NT (sometimes problems with dislog.txt?)
 @rem bc -lq prims.txt dislog.txt ecm.txt quadRest.txt sieb.txt seed
+@echo Use  quit  to exit
 bc -lq prims.txt dislog.txt ecm.txt quadRest.txt seed
+
 
--- setup/template/Bc/BCmin.bat
+++ setup/template/Bc/BCmin.bat
@@ -3,8 +3,9 @@ IF EXIST BC.exe GOTO OK
 
 Z:
 cd ..
-cd \Zahlentheorie\BC
+cd \Numbertheory\BC
 
 :OK
 bc -lq prims.txt calc.txt | find /V "Poll" | find /V "|" | sort > res.txt
 exit
+
